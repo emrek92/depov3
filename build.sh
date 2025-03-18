503bf5d489403c7027ff96567b55d14dfcb34565
emrek92 <emrelinho@gmail.com>
2025-03-18 10:39:08 +0300

    Build script güncellendi, pip upgrade komutu eklendi

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# Pip'i güncelleyelim
+python -m pip install --upgrade pip
+
+# Gereksinimleri yükleyelim
 pip install -r requirements.txt
 
+# Statik dosyaları toplatalım
 python manage.py collectstatic --no-input
+
+# Veritabanı migrasyonlarını uygulayalım
 python manage.py migrate 
